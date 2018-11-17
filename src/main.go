31a17a6b4729a5a8412e91723b73bb4693fe2ac7
Kasper Olsen <root@kols.dk>
2018-11-18 00:21:45 +0100

    added command line args for ip and bulb id

--- src/main.go
+++ src/main.go
@@ -5,19 +5,20 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"os"
+	"strconv"
 	"sync"
 	"time"
 )
 
-func setLight(lightID int) {
-	url := fmt.Sprintf("http://192.168.1.23/api/fqaj6KtOHPSNpPzTzFy4g5rce98CIf2uAFlXbViH/lights/%d/state", lightID)
-
+func setLight(ip string, lightID int) {
+	url := fmt.Sprintf("http://%s/api/fqaj6KtOHPSNpPzTzFy4g5rce98CIf2uAFlXbViH/lights/%d/state", ip, lightID)
 	for {
 		rand.Seed(time.Now().UnixNano())
 		hue := rand.Intn(65535)
 		fmt.Printf("Light %d - hue: %d\n", lightID, hue)
-		str := fmt.Sprintf(`{"on":true, "sat":254, "bri":100,"hue":%d}`, hue)
-		jsonStr := []byte(str)
+		body := fmt.Sprintf(`{"on":true, "sat":254, "bri":100,"hue":%d}`, hue)
+		jsonStr := []byte(body)
 		req, err := http.NewRequest("PUT", url, bytes.NewBuffer(jsonStr))
 
 		client := &http.Client{}
@@ -33,17 +34,30 @@ func setLight(lightID int) {
 
 func main() {
 
-	lights := [...]int{1, 3, 6}
+	fmt.Println(len(os.Args))
+	if len(os.Args) < 3 {
+		fmt.Println("Bad arguments. Please provide ip of Hue Bridge and at least 1 bulb id.")
+		return
+	}
+	var lights []int
+	for i := 2; i < len(os.Args); i++ {
+		id, err := strconv.Atoi(os.Args[i])
+
+		if err != nil {
+			panic(err)
+		}
+
+		lights = append(lights, id)
+	}
 
 	var wg sync.WaitGroup
 	wg.Add(len(lights))
 
 	for _, i := range lights {
 		go func(lightID int) {
-			setLight(lightID)
+			setLight(os.Args[1], lightID)
 			wg.Done()
 		}(i)
-		time.Sleep(time.Millisecond * 200)
 	}
 	wg.Wait()
 }
